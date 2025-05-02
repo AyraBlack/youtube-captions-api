e39d47669a537c1ceab376d39090ce897f6ae501
AyraBlack <anna.udriste@gmail.com>
2025-05-02 20:16:29 +0300

    Update captions_server.py

--- captions_server.py
+++ captions_server.py
@@ -15,5 +15,8 @@ def get_captions():
     except Exception as e:
         return jsonify({ "error": str(e) }), 400
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
